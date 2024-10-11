c9b91829e984295741d3a4375b358fac5f06eb27
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-10-11 14:28:56 +0200

    initial project

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const roboto = localFont({
+    src: [
+        {
+            path: "./fonts/Roboto-Regular.woff",
+            style: "normal",
+            weight: "400",
+        },
+        {
+            path: "./fonts/Roboto-Medium.woff",
+            style: "normal",
+            weight: "500",
+        },
+        {
+            path: "./fonts/Roboto-Bold.woff",
+            style: "normal",
+            weight: "700",
+        },
+        {
+            path: "./fonts/Roboto-Italic.woff",
+            style: "italic",
+            weight: "400",
+        },
+    ],
+    variable: "--font-roboto",
+});
+
+export const metadata: Metadata = {
+    title: "LearnLingo - Find Your Ideal Language Teacher",
+    description: "LearnLingo connects you with expert language teachers to help you achieve your language learning goals. Browse profiles, read reviews, and start learning today!",
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en">
+        <body className={`${roboto.variable} antialiased`}>
+            {children}
+        </body>
+        </html>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+export default function Page() {
+  return (
+    <div className="">
+      <header className="">
+        <nav className="">
+            
+        </nav>
+      </header>
+
+      <main className="">
+
+      </main>
+
+      <footer className="">
+
+      </footer>
+    </div>
+  );
+}
