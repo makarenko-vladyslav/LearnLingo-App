279a3f29c68798d3b176e400c89780bd2a35059b
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-10-21 18:20:42 +0200

    add push message if user not logged in

--- src/components/FavoriteBtn.tsx
+++ src/components/FavoriteBtn.tsx
@@ -2,6 +2,9 @@ import { FaRegHeart, FaHeart } from "react-icons/fa6";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 import { selectFavoriteTeachers, Teacher, toggleFavoriteTeacher } from "../redux/teachersSlice";
+import { selectIsAuthenticated } from "../redux/authSlice";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface HaveYouATeacherForMe {
     teacher?: Teacher;
@@ -10,12 +13,20 @@ interface HaveYouATeacherForMe {
 export default function FavoriteBtn({ teacher }: HaveYouATeacherForMe) {
     const dispatch = useDispatch();
     const router = useRouter();
-
+    const isAuthenticated = useSelector(selectIsAuthenticated);
     const favoriteTeachers = useSelector(selectFavoriteTeachers);
 
     const isFavorited = teacher ? favoriteTeachers.some((favTeacher) => favTeacher.id === teacher.id) : false;
 
     const handleClick = () => {
+        if (!isAuthenticated) {
+            toast.warning("Please log in to save items to your favorites", {
+                position: "top-center",
+                autoClose: 3000,
+            });
+            return;
+        }
+
         if (teacher) {
             dispatch(toggleFavoriteTeacher(teacher));
         } else {
@@ -24,20 +35,23 @@ export default function FavoriteBtn({ teacher }: HaveYouATeacherForMe) {
     };
 
     return (
-        <button
-            className="flex justify-center items-center gap-2 font-medium"
-            onClick={handleClick}>
-            {teacher ? (
-                isFavorited ? (
+        <>
+            <button
+                className="flex justify-center items-center gap-2 font-medium"
+                onClick={handleClick}>
+                {teacher ? (
+                    isFavorited ? (
+                        <FaHeart className="text-buttonHover text-2xl hover:text-primary transition-colors duration-150" />
+                    ) : (
+                        <FaRegHeart className="text-text text-2xl hover:text-buttonHover transition-colors duration-150" />
+                    )
+                ) : favoriteTeachers.length > 0 ? (
                     <FaHeart className="text-buttonHover text-2xl hover:text-primary transition-colors duration-150" />
                 ) : (
                     <FaRegHeart className="text-text text-2xl hover:text-buttonHover transition-colors duration-150" />
-                )
-            ) : favoriteTeachers.length > 0 ? (
-                <FaHeart className="text-buttonHover text-2xl hover:text-primary transition-colors duration-150" />
-            ) : (
-                <FaRegHeart className="text-text text-2xl hover:text-buttonHover transition-colors duration-150" />
-            )}
-        </button>
+                )}
+            </button>
+            <ToastContainer />
+        </>
     );
 }
